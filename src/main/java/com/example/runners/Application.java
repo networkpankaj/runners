feb182b0ed4ad0a229d4b7b758a06b9112118514
@networkpankaj <@networkpankaj>
2024-06-03 09:45:15 +0000

    Initialize

---
+++ src/main/java/com/example/runners/Application.java
@@ -0,0 +1,13 @@
+package com.example.runners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnersApplication.class, args);
+	}
+
+}
